0f2444807a2cb2aa5f1dca440c159366fe70c33c
Ben Pringle <benjamin.m.pringle@gmail.com>
2023-04-05 14:04:22 -0400

    modify build scripts to allow for easy compilation of other apps with this engine.

--- build.py
+++ build.py
@@ -1,46 +1,116 @@
 import sys
 import subprocess
+import os
 
-sys.path += ["./scripts"]
+sys.path += [os.path.realpath(sys.path[0]) + "/scripts"]
 
 from executeCommand import execCmd
 
-def clean_main_project():
-    print("Cleaning Main Project ...")
-    execCmd("scons -j8 -c", out = None)
-    execCmd("rm -rf deploy/*")
+APP_NAME = "TEST_APP"
+EXECUTABLE_FILE_PATH="./bin/rel/tstGame"
+EXECUTABLE_NAME="tstGame"
+DEPLOY_FOLDER="./deploy"
+PROJECT_BUILD_FOLDER="./"
+PROJECT_BUILD_CMD="scons -j8"
+PROJECT_CLEAN_CMD="scons -j8 -c; rm -rf deploy/*"
+BUILD_TYPE = "T"
+
+def clean_all_projects():
+    clean_project(os.path.realpath(sys.path[0]))
+    if BUILD_TYPE == "O":
+        clean_project(PROJECT_BUILD_FOLDER)
+
+def clean_project(project_dir):
+    print(f'Cleaning Project In Directory {project_dir}...')
+    execCmd(PROJECT_CLEAN_CMD, cwdOverride = project_dir, out = None)
     print("Done Cleaning")
 
-def build_main_project():
-   print("Building Main Project ...")
-   execCmd("scons -j8", out = None)
-   print("Done Building Main Project")
+def build_all_projects():
+    build_project(os.path.realpath(sys.path[0]))
+    if BUILD_TYPE == "O":
+        build_project(PROJECT_BUILD_FOLDER)
+
+def build_project(project_dir):
+   print(f'Building Project In Directory {project_dir}...')
+   execCmd(PROJECT_BUILD_CMD, cwdOverride = project_dir, out = None)
+   print("Done Building Project")
 
 def build_dependencies():
     print("Building Main Project Dependencies ...")
-    execCmd(f'{sys.executable} scripts/compileDeps.py', out = None)
+    execCmd(f'{sys.executable} {os.path.realpath(sys.path[0])}/scripts/compileDeps.py', out = None)
     print("Done Building Main Project Dependencies")
 
 def compile_shaders():
     print("Compiling Shaders ...")
-    execCmd("sh scripts/compileShaders.sh", out = None)
+    execCmd(f'sh {os.path.realpath(sys.path[0])}/scripts/compileShaders.sh', out = None)
     print("Done Compiling Shaders")
 
 def post_compile_scripts():
     print("Running Post-Compile Scripts ...")
     print("\nPlease note that install_name_tool will likely give errors on subsquent calls to this script. They are non-fatal and can be safely ignored.\n")
-    execCmd("sh scripts/postCompileCommands.sh", out = None)
+    execCmd(
+        f'''
+        export APP_NAME={APP_NAME};
+        export EXECUTABLE_FILE_PATH={EXECUTABLE_FILE_PATH};
+        export EXECUTABLE_NAME={EXECUTABLE_NAME};
+        export DEPLOY_FOLDER={DEPLOY_FOLDER}; 
+        sh {os.path.realpath(sys.path[0])}/scripts/postCompileCommands.sh
+        ''', out = None)
     print("Done Running Post-Compile Scripts")
 
 def build_all():
-    clean_main_project()
+    clean_all_projects()
     build_dependencies()
-    build_main_project()
+    build_all_projects()
     compile_shaders()
     post_compile_scripts()
 
+def set_test_project_env():
+    print("Setting Test Project Environment ...")
+    print("Done Setting Test Project Environment")
+
+def set_other_project_env():
+    print("Setting Other Project Environment ...")
+    APP_NAME = input("Set app name:")
+    EXECUTABLE_FILE_PATH = input("Set executable file path:")
+    EXECUTABLE_NAME = input("Set executable name:")
+    DEPLOY_FOLDER = input("Set deploy folder:")
+    BUILD_TYPE = "O"
+    print("Done Setting Other Project Environment")
+    
 def main():
-    build_setting = ""
+
+    build_dictionary = {
+        'C': clean_all_projects,
+        'B': build_all_projects,
+        'D': build_dependencies,
+        'S': compile_shaders,
+        'P': post_compile_scripts,
+        'A': build_all
+    }
+
+    build_type_dictionary = {
+        'T': set_test_project_env,
+        'O': set_other_project_env
+    }
+
+    build_type = input(
+        '''
+        Build Type:
+        Do you want to target the test project or do you want to target your own project using this engine?
+        \tT - Test Project
+        \tO - Other Project
+        '''
+    )
+
+    if len(build_type) == 0:
+        print("Error: no build type selected. Exiting Build Script ...")
+        exit()
+
+    if len(build_type) > 1:
+        print("Warning: Only the first option passed will be used, all subsequent ones will be ignored")
+
+    build_type_dictionary[build_type[0]]()
 
     build_setting = input(
         '''
@@ -53,16 +123,6 @@ def main():
         \tA - runs all build commands in order'
         '''
     )
-
-    build_dictionary = {
-        'C': clean_main_project,
-        'B': build_main_project,
-        'D': build_dependencies,
-        'S': compile_shaders,
-        'P': post_compile_scripts,
-        'A': build_all
-
-    }
 
     if 'A' in build_setting:
         if len(build_setting) > 1:
--- scripts/postCompileCommands.sh
+++ scripts/postCompileCommands.sh
@@ -5,37 +5,40 @@ function replace() {
     sed -i '' 's|'$2'|'$3'|' $1
 }
 
-APP_NAME=TEST_APP
-EXECUTABLE_NAME=tstGame
-ICD_FILE_PATH=deploy/$APP_NAME.app/Contents/Resources/vulkan/icd.d/MoltenVK_icd.json
-PLIST_FILE_PATH=deploy/$APP_NAME.app/Contents/Info.plist
+APP_NAME="${APP_NAME:-TEST_APP}"
+EXECUTABLE_FILE_PATH="${EXECUTABLE_FILE_PATH:-./bin/rel/tstGame}"
+EXECUTABLE_NAME="${EXECUTABLE_NAME:-tstGame}"
+DEPLOY_FOLDER="${DEPLOY_FOLDER:-./deploy}"
+
+ICD_FILE_PATH=$DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources/vulkan/icd.d/MoltenVK_icd.json
+PLIST_FILE_PATH=$DEPLOY_FOLDER/$APP_NAME.app/Contents/Info.plist
 
 #construct app bundle
 
-mkdir -p deploy
-rm -rf deploy/$APP_NAME.app
-mkdir -p deploy/$APP_NAME.app
-mkdir -p deploy/$APP_NAME.app/Contents
-mkdir -p deploy/$APP_NAME.app/Contents/MacOS
-mkdir -p deploy/$APP_NAME.app/Contents/Resources
-mkdir -p deploy/$APP_NAME.app/Contents/Resources/vulkan
-mkdir -p deploy/$APP_NAME.app/Contents/Frameworks
+mkdir -p $DEPLOY_FOLDER
+rm -rf $DEPLOY_FOLDER/$APP_NAME.app
+mkdir -p $DEPLOY_FOLDER/$APP_NAME.app
+mkdir -p $DEPLOY_FOLDER/$APP_NAME.app/Contents
+mkdir -p $DEPLOY_FOLDER/$APP_NAME.app/Contents/MacOS
+mkdir -p $DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources
+mkdir -p $DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources/vulkan
+mkdir -p $DEPLOY_FOLDER/$APP_NAME.app/Contents/Frameworks
 
-cp artifacts/Info.plist deploy/$APP_NAME.app/Contents/Info.plist
-cp artifacts/vk_engine.icns deploy/$APP_NAME.app/Contents/Resources/vk_engine.icns
-cp -r assets/ deploy/$APP_NAME.app/Contents/Resources/assets
-cp -r shaders/ deploy/$APP_NAME.app/Contents/Resources/shaders
-cp -r VulkanSDKMacOS/macOS/share/vulkan/icd.d deploy/$APP_NAME.app/Contents/Resources/vulkan/icd.d
-cp bin/rel/$EXECUTABLE_NAME deploy/$APP_NAME.app/Contents/MacOS/$EXECUTABLE_NAME
-cp VulkanSDKMacOS/macOS/lib/libMoltenVK.dylib deploy/$APP_NAME.app/Contents/Frameworks/libMoltenVK.dylib
-cp VulkanSDKMacOS/macOS/lib/libvulkan.1.3.243.dylib deploy/$APP_NAME.app/Contents/Frameworks/libvulkan.1.dylib
-cp bin/rel/libVulkanEngineLib.dylib deploy/$APP_NAME.app/Contents/Frameworks/libVulkanEngineLib.dylib
+cp artifacts/Info.plist $DEPLOY_FOLDER/$APP_NAME.app/Contents/Info.plist
+cp artifacts/vk_engine.icns $DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources/vk_engine.icns
+cp -r assets/ $DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources/assets
+cp -r shaders/ $DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources/shaders
+cp -r VulkanSDKMacOS/macOS/share/vulkan/icd.d $DEPLOY_FOLDER/$APP_NAME.app/Contents/Resources/vulkan/icd.d
+cp $EXECUTABLE_FILE_PATH $DEPLOY_FOLDER/$APP_NAME.app/Contents/MacOS/$EXECUTABLE_NAME
+cp VulkanSDKMacOS/macOS/lib/libMoltenVK.dylib $DEPLOY_FOLDER/$APP_NAME.app/Contents/Frameworks/libMoltenVK.dylib
+cp VulkanSDKMacOS/macOS/lib/libvulkan.1.3.243.dylib $DEPLOY_FOLDER/$APP_NAME.app/Contents/Frameworks/libvulkan.1.dylib
+cp bin/rel/libVulkanEngineLib.dylib $DEPLOY_FOLDER/$APP_NAME.app/Contents/Frameworks/libVulkanEngineLib.dylib
 
 #change rpath and dylib locations
-install_name_tool -add_rpath @loader_path/../Frameworks deploy/$APP_NAME.app/Contents/MacOS/$EXECUTABLE_NAME
-install_name_tool -add_rpath @loader_path/../Frameworks deploy/$APP_NAME.app/Contents/Frameworks/libVulkanEngineLib.dylib
-install_name_tool -id libVulkanEngineLib.dylib deploy/$APP_NAME.app/Contents/Frameworks/libVulkanEngineLib.dylib
-install_name_tool -change bin/rel/libVulkanEngineLib.dylib @rpath/libVulkanEngineLib.dylib deploy/$APP_NAME.app/Contents/MacOS/$EXECUTABLE_NAME
+install_name_tool -add_rpath @loader_path/../Frameworks $DEPLOY_FOLDER/$APP_NAME.app/Contents/MacOS/$EXECUTABLE_NAME
+install_name_tool -add_rpath @loader_path/../Frameworks $DEPLOY_FOLDER/$APP_NAME.app/Contents/Frameworks/libVulkanEngineLib.dylib
+install_name_tool -id libVulkanEngineLib.dylib $DEPLOY_FOLDER/$APP_NAME.app/Contents/Frameworks/libVulkanEngineLib.dylib
+install_name_tool -change bin/rel/libVulkanEngineLib.dylib @rpath/libVulkanEngineLib.dylib $DEPLOY_FOLDER/$APP_NAME.app/Contents/MacOS/$EXECUTABLE_NAME
 
 replace $PLIST_FILE_PATH INSERT_EXECUTABLE_NAME $EXECUTABLE_NAME
 replace $PLIST_FILE_PATH INSERT_DISPLAY_NAME $APP_NAME
